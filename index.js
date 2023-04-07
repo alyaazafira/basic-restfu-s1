5632d6fa128a30fccb25c28d01537659b4d27913
alyaazafira <yazap73@gmail.com>
2023-04-07 09:35:19 +0800

    login & register. api

--- index.js
+++ index.js
@@ -2,15 +2,50 @@ const express = require('express')
 const app = express()
 const port  = 3000
 
+let dbUser = [
+  { 
+    username: "alyaa",
+    password: "12345",
+    email: "alyaa@gmail.com"
+  }
+]
+app.use(express.json());
 
+//create GET route
 app.get('/', (req, res) => {
   res.send('hello world')
-})
+});
 
-app.post('/bye', (req, res) => {
+app.get('/bye', (req, res) => {
     res.send('BYE BYE WORLD')
-})
+});
 
 app.listen(port,() => {
     console.log(' Example app listening on port')
+});
+
+app.post('/login', (req,res) => {
+  //get the usename and password from req
+  const { usernsme, password } = req.body;
+
+  //find usser in the database
+  const user = dbUser.find(user => user.username === usernsme && user.password === password);
+app.post('/register', (req, res)=>
+    let  dbUser = req.body
+    res.send(
+      register(
+        dbUser.username,
+        dbUser.password,
+        dbUser.name,
+        dbUser.email
+      )
+    ))
+  // if user is found
+  if (user) {
+      res. send (user);
+
+  } else {
+  //if user is not found.
+res.send({ error: "User not found" });
+}
 })
