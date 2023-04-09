83973f7704251242ce71524d0d3c6a4ed2255dea
alyaazafira <yazap73@gmail.com>
2023-04-09 14:54:18 +0800

    login and register

--- index.js
+++ index.js
@@ -1,51 +1,110 @@
-const express = require('express')
-const app = express()
-const port  = 3000
+const express = require('express');
+const app = express();
+const port  = 4000
 
 let dbUser = [
   { 
     username: "alyaa",
     password: "12345",
     email: "alyaa@gmail.com"
+  },
+  {
+    username: "apihazman",
+    password: "66666",
+    email: "apiz@gmail.com"
   }
 ]
+//anable json body parsing
 app.use(express.json());
 
 //create GET route
-app.get('/', (req, res) => {
-  res.send('hello world')
-});
-
-app.get('/bye', (req, res) => {
-    res.send('BYE BYE WORLD')
-});
-
-app.listen(port,() => {
-    console.log(' Example app listening on port')
-});
-
 app.post('/login', (req,res) => {
-  //get the usename and password from req
-  const { usernsme, password } = req.body;
+  //get the username and password from req
+  //const { username, password } = req.body;//
+  let data = req.body
+  res.send(
+    login(
+      data.username,
+      data.password
+    )
+  );
+    });
 
+    app.use(express.json());
   //find usser in the database
-  const user = dbUser.find(user => user.username === usernsme && user.password === password);
-app.post('/register', (req, res)=>
-    let  dbUser = req.body
+ // const user = dbUser.find(user => user.username === username && user.password === password);
+app.post('/register', (req, res)=>{
+    let  data = req.body
     res.send(
       register(
-        dbUser.username,
-        dbUser.password,
-        dbUser.name,
-        dbUser.email
+        data.username,
+        data.password,
+        data.email
       )
-    ))
-  // if user is found
-  if (user) {
-      res. send (user);
+    );
+      });
 
+      app.get('/', (req, res) => {
+        res.send('Hello World!')
+      })
+      
+      app.get('/bye', (req, res) => {
+          res.send('Bye Bye World!')
+        })
+
+
+app.listen(port,() => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+//creat a post route for user to log in
+app.post('/login', (req, res) => {
+  // get the username and pasword from the request body
+  const { username, password } =req.body;
+
+  // find the username in the database
+  const user = dbUser.find(user => user.username === uesrname && user.password === password);
+
+  // if
+  if (user) {
+    res.send(user);
   } else {
-  //if user is not found.
-res.send({ error: "User not found" });
-}
+    // if user is not found, return an error message
+    res.send({ error: "User not found "});
+  }
 })
+
+
+function login (username, password){
+  console.log("someone try to login with", username, password)
+  let matched =dbUser.find(element =>
+    element.username == username
+)
+if(matched){
+    if(matched.password == password){
+        return matched
+    }else{
+        return "Password not matched"
+    }
+}
+else {
+  return "Username not found"
+}
+}
+
+function register(
+  newusername,
+  newpassword,
+  newemail
+){
+
+dbUser.find(element => {
+  console.log(element)
+})
+dbUser.push({
+  username : newusername,
+  password : newpassword,
+  email : newemail,
+})
+ return "New account has been created"
+}
